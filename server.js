334286fc5a2e30fd4d45be124949d7a5c00364f8
Dustin Myers <dstnmyrs99@yahoo.com>
2020-10-15 10:13:19 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log('Server listening on http://localhost:8080');
 });
